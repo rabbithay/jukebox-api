4fc37c1b49db3109a7bed3751c2c33efb8ed1442
rabbithay <thay.csb@gmail.com>
2021-12-05 02:56:15 -0300

    test: add new song factory

---
+++ tests/factories/generateNewSongObject.js
@@ -0,0 +1,8 @@
+import faker from 'faker';
+
+const newSongBody = {
+  name: faker.datatype.string(),
+  youtubeLink: 'https://www.youtube.com/watch?v=ZEkfsSms4Q0&ab_channel=BillieEilishVEVO',
+};
+
+export default newSongBody;
