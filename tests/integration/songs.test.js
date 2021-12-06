e160ad77db012dde563d5e69d56155570084d89e
rabbithay <thay.csb@gmail.com>
2021-12-06 14:40:29 -0300

    test: integration of post /recomendations/:id/downvote route

--- tests/integration/songs.test.js
+++ tests/integration/songs.test.js
@@ -31,6 +31,38 @@ describe('POST /recommendations', () => {
   });
 });
 
+describe('POST /recommendations/:id/upvote', () => {
+  it('should answer with status 200 for valid id', async () => {
+    const id = await addSongIntoDatabaseReturningId();
+
+    const response = await supertest(app).post(`/recommendations/${id}/upvote`);
+
+    expect(response.status).toBe(200);
+  });
+
+  it('should answer with status 404 for invalid id', async () => {
+    const response = await supertest(app).post(`/recommendations/${NaN}/upvote`);
+
+    expect(response.status).toBe(404);
+  });
+});
+
+describe('POST /recommendations/:id/downvote', () => {
+  it('should answer with status 200 for valid id', async () => {
+    const id = await addSongIntoDatabaseReturningId();
+
+    const response = await supertest(app).post(`/recommendations/${id}/downvote`);
+
+    expect(response.status).toBe(200);
+  });
+
+  it('should answer with status 404 for invalid id', async () => {
+    const response = await supertest(app).post(`/recommendations/${NaN}/downvote`);
+
+    expect(response.status).toBe(404);
+  });
+});
+
 afterAll(async () => {
   await connection.end();
 });
