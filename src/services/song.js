fc8f0fac06a1f97d25b90751ef2706ee6ac8092c
rabbithay <thay.csb@gmail.com>
2021-12-06 04:48:17 -0300

    feat: create get recommendations services

--- src/services/song.js
+++ src/services/song.js
@@ -1,33 +1,46 @@
-/* eslint-disable no-unused-vars */
 import * as songRepository from '../repositories/song';
-import addNewSongSchema from '../schemas/addNewSongSchema';
 
-export function isYoutubeLink({ name, youtubeLink }) {
-  const validation = addNewSongSchema.validate({ name, youtubeLink });
+export function validateObject({ object, schema }) {
+  const validation = schema.validate(object);
   return !validation.error;
 }
 
 export async function addNewSong({ name, youtubeLink }) {
   const input = { name, youtubeLink, rating: 0 };
-  await songRepository.insertSong(input);
+  return await songRepository.insertSong(input);
 }
 
-export async function songExist(id) {
+export async function checkSongExist(id) {
   const song = await songRepository.getSongById(id);
   return !!song;
 }
 
 export async function updateScore({ id, vote }) {
   if (vote === 'up') {
-    await songRepository.updateScore({ id, vote: 1 });
-    return;
+    return await songRepository.updateScore({ id, vote: 1 });
   }
 
-  const { score } = await songRepository.getSongById(id);
-  if (score < -4) {
-    await songRepository.deleteSong(id);
-    return;
+  const { song_score } = await songRepository.getSongById(id);
+  if (song_score <= -4) {
+    return await songRepository.deleteSong(id);
   }
 
-  await songRepository.updateScore({ id, vote: -1 });
+  return await songRepository.updateScore({ id, vote: -1 });
+}
+
+export async function getRandomRecommendation() {
+  const rating = (Math.random() <= 0.30)
+    ? 'lower'
+    : 'higher';
+
+  const recommendation = await songRepository.getRecommendation({ rating });
+
+  if (!recommendation) return await songRepository.getRecommendation();
+
+  return recommendation;
+}
+
+export async function getListOfTopSongs(amount) {
+  const bestSongs = await songRepository.getSongsByScore(amount);
+  return bestSongs;
 }
