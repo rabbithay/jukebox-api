e8b448b63eb05d51f82a7023c5b81dbc13af4fba
rabbithay <thay.csb@gmail.com>
2021-12-06 04:48:56 -0300

    fix: update songs schemas

--- src/schemas/addNewSongSchema.js
+++ src/schemas/addNewSongSchema.js
@@ -1,9 +1,8 @@
 import Joi from 'joi';
 
-// eslint-disable-next-line import/prefer-default-export
 export const addNewSongSchema = Joi.object({
   name: Joi.string().required(),
   youtubeLink: Joi.string()
-    .pattern(/^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$/)
+    .pattern(/^(https?:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$/)
     .required(),
 });
