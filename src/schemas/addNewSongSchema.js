a4a03151240a781bc451d8a7599a57529df8450b
Bruno <mguimaraesbruno@gmail.com>
2021-12-07 14:39:27 -0300

    Code Review: Brunão

--- src/schemas/addNewSongSchema.js
+++
@@ -1,8 +0,0 @@
-import Joi from 'joi';
-
-export const addNewSongSchema = Joi.object({
-  name: Joi.string().required(),
-  youtubeLink: Joi.string()
-    .pattern(/^(https?:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$/)
-    .required(),
-});
--- tests/factories/generateCreatedSongObject.js
+++
@@ -1,8 +0,0 @@
-import faker from 'faker';
-
-export const songBody = {
-  id: faker.datatype.number(),
-  name: faker.datatype.string(),
-  youtubeLink: 'https://www.youtube.com/watch?v=ZEkfsSms4Q0&ab_channel=BillieEilishVEVO',
-  score: faker.datatype.number(),
-};
